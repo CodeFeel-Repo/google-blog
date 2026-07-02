c9b6986a4a21cdcb42873d7d599918401c88dbd5
CodeFeel-Repo <tftfent@gmail.com>
2026-07-03 03:22:32 +0900

    First Commit - Google-Blog Typing

---
+++ code-block.js
@@ -0,0 +1,19 @@
+function copyCode(button) {
+  const container = button.closest('.code-container');
+  const codeElement = container.querySelector('.code-content code');
+  const textToCopy = codeElement.innerText;
+
+  navigator.clipboard.writeText(textToCopy).then(() => {
+    const originalText = button.innerText;
+    button.innerText = '복사 완료!';
+    button.style.backgroundColor = '#388E3C';
+
+    setTimeout(() => {
+      button.innerText = originalText;
+      button.style.backgroundColor = '#4CAF50';
+    }, 2000);
+  }).catch(err => {
+    console.error('클립보드 복사 실패:', err);
+    alert('복사에 실패했습니다.');
+  });
+}
